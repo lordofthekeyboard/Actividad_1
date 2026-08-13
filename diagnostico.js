dac5fb2694c6e8c6ed6eec382124fe9957a343fd
lordofthekeyboard <lordofthekeyboard@gmail.com>
2026-08-12 21:08:36 -0300

    https://github.com/lordofthekeyboard/Actividad_1.git

---
+++ diagnostico.js
@@ -0,0 +1,8 @@
+//Version del Node.js
+console.log (process.version);
+//Version de la plataforma
+console.log (process.platform);
+//Version del argv
+console.log (process.argv);
+//Version del env
+console.log (process.env)
